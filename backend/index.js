d665e67b06a1b4950397db4765b6006007c2b8b9
Felipe Oliveira <felipe.boliveira@outlook.com>
2020-01-19 00:27:35 -0300

    Configura a base de dados

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-bh7zb.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(express.json());
 
