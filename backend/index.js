a0b97610bf5175071c0a0b013dc6b12437be5467
Felipe Oliveira <felipe.boliveira@outlook.com>
2020-01-19 00:15:38 -0300

    Configuracao inicial

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,24 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
-  return response.send('Hello World');
+  return response.json({ message: 'Hello World'});
 });
 
 app.listen(3333);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para a criação ou alteração de um registro)
+
+// Logs
+// console.log(request.query);
+
+// MongoDB (Não-relacional)
+// Bom para aplicações que não possuam muitos relacionamentos em banco
