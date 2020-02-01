4ff8a44abe3d6809866f3da2bae9221b30d84a69
Felipe Oliveira <felipe.boliveira@outlook.com>
2020-02-01 13:27:16 -0300

    Adiciona form html

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
 
+import './global.css';
+import './App.css';
+
 function App() {
 
   return (
     <div id='app'>
       <asside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
 
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
       </asside>
       <main>
-        
+
       </main>
-    </div>    
+    </div>
   );
 }
 
