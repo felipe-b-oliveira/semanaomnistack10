9387753676c5c09eaf10621008fb350f23dfd7e4
Felipe Oliveira <felipe.boliveira@outlook.com>
2020-03-12 22:12:30 -0300

    Adiciona navegacao para a pagina Profile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,7 +5,7 @@ import MapView, { Marker, Callout } from 'react-native-maps';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
-function Main() {
+function Main({ navigation }) {
   const [currentRegion, setCurrentRegion] = useState(null);
 
   // Localização do usuario
@@ -43,7 +43,9 @@ function Main() {
           style={styles.avatar}
           source={{ uri:'https://scontent-gig2-1.xx.fbcdn.net/v/t1.0-9/12993339_1538337406471451_8230930593483038980_n.jpg?_nc_cat=105&_nc_sid=7aed08&_nc_ohc=0Zr01VfkM-4AX9AqAxe&_nc_ht=scontent-gig2-1.xx&oh=5ea78b98452ce3740e22fc7f540d1513&oe=5E987573' }} 
         />
-        <Callout>
+        <Callout onPress={() => {
+          navigation.navigate('Profile', { github_username: 'felipe-b-oliveira' })
+        }}>
           <View style={styles.callout}>
             <Text style={styles.devName}>Giselle Oliveira</Text>
             <Text style={styles.devBio}>Desenvolvedora Front-end</Text>
