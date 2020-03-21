50652acbd390529612bde90a3a68f7e20050fcfb
Felipe Oliveira <felipe.boliveira@outlook.com>
2020-03-21 19:02:59 -0300

    backend: Adiciona websocket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 // Constantes são usadas em variáveis que não se alteram
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-bh7zb.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -16,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
 
 // Métodos HTTP: GET, POST, PUT, DELETE
 
